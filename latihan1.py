33a7299d83e18b0789acb8de4802079b8eaf8ea9
ade maulani bilgis <iiigis0422@gmail.com>
2021-11-10 15:04:47 +0700

    menambahkan file

---
+++ latihan1.py
@@ -0,0 +1,6 @@
+a = input("masukan angka: ")
+i =input ("masukan angka: ")
+if a>i:
+    print("lebih besar",a,"dari pada",i)
+if i>a:
+    print("lebih besar",i,"dari pada",a)
---
+++ latihan2.py
@@ -0,0 +1,9 @@
+a=input("masukan data ke-1: ")
+b=input("masukan data ke-2: ")
+c=input("masukan data ke-3: ")
+if a>b>c:
+    print("urutan bilangan",a,b,c)
+if b>a>c:
+    print("urutan bilangan",b,a,c)
+else :
+    print("urutan bilangan",c,b,a)
